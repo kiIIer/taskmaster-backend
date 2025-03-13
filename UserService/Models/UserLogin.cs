c3f9bacf272f2c61a10491d5e5dee2732212aa8e
kiIIer <michael.molchanov.2004@gmail.com>
2025-03-13 09:04:25 +0100

    created models for user service

---
+++ UserService/Models/UserLogin.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Models;
+
+public class UserLogin
+{
+    [Required] public string Email { get; set; }
+
+    [Required] public string Password { get; set; }
+}
